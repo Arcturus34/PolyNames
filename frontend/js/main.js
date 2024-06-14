30ff4e220a75f2450f8e59fc3953a5c1f03aac11
Arcturus34 <supersonic343434@gmail.com>
2024-06-14 20:45:22 +0200

    Ajout d'une page avec et sans couleurs

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,5 +1,4 @@
 import { ApplicationPolyname } from "./application/application-polyname.js";
-import { CardsService } from "./services/cards-service.js";
 
 window.addEventListener("load", () => {
     const app = new ApplicationPolyname();
